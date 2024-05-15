fa539408e4120ac71d6c4ff1bd35e6bf1b4109ca
unknown <ikebelida156@gmail.com>
2024-05-15 10:05:42 -0700

    updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 const salt = bcrypt.genSaltSync(10)
 const secret = 'asdflkjhg'
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'https://rtblog-com.onrender.com' }))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
